f40bd837fc9c8128a380d3aaea33777909fcf5b9
lenovo <smartymohit9@gmail.com>
2017-05-06 18:32:55 +0530

    second commit

--- app/src/main/java/mohit/transporter/MainActivity.java
+++ app/src/main/java/mohit/transporter/MainActivity.java
@@ -1,13 +1,21 @@
 package mohit.transporter;
-
 import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-TextView text;
+public class MainActivity extends AppCompatActivity implements LocationListener {
+    Button getLocationBtn;
+    TextView locationText;
+    Location location;
+
     LocationManager locationManager;
 
     @Override
@@ -15,12 +23,52 @@ TextView text;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    text= (TextView) findViewById(R.id.textView);
+       // getLocationBtn = (Button)findViewById(R.id.getLocationBtn);
+        locationText = (TextView)findViewById(R.id.textView);
+        getLocation();
 
+    }
 
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+    void getLocation() {
+        try {
+            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            Criteria criteria = new Criteria();
+            String bestProvider = locationManager.getBestProvider(criteria, true);
+            location = locationManager.getLastKnownLocation(bestProvider);
+            
+            if (location != null) {
+                locationText.setText("Current Location: " + location.getLatitude() + ", " + location.getLongitude());
+                onLocationChanged(location);
+            }
+            locationManager.requestLocationUpdates(bestProvider, 20000, 0, this);
+            Toast.makeText(getApplicationContext(),"hi ",Toast.LENGTH_LONG).show();
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, this);
+            locationText.setText("Current Location: " + location.getLatitude() + ", " + location.getLongitude());
+        }
+        catch(SecurityException e) {
+            e.printStackTrace();
+        }
+    }
 
-        //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, getApplicationContext());
+    @Override
+    public void onLocationChanged(Location location) {
+        Toast.makeText(getApplicationContext(),"hi12 ",Toast.LENGTH_LONG).show();
+        locationText.setText("Current Location: " + location.getLatitude() + ", " + location.getLongitude());
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(MainActivity.this, "Please Enable GPS and Internet", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
 
     }
 }
